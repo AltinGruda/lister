e58e96a8a9018a383ba813c686d9faf4c6f68977
AltinGruda <altingruda@gmail.com>
2023-05-30 18:40:13 +0200

    connect to mongodb

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+// decide later if you want (session) for authentication
+const MongoStore = require("connect-mongo") //(session);
+const connectDb = require('./config/database')
 require("dotenv").config({ path: "./config/.env" });
 
+connectDb();
 app.use('/', (req, res) => {
     res.send('Hi')
 })
