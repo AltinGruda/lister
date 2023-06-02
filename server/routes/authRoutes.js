b8593d7cdcda161e38d1ccc83897fc3221a8d7f4
AltinGruda <altingruda@gmail.com>
2023-06-03 01:03:09 +0200

    google auth login successful

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,6 +4,18 @@ require("dotenv").config({ path: "./config/.env" });
 
 //different than 100dev
 
+router.get("/login/success", (req, res) => {
+	if (req.user) {
+		res.status(200).json({
+			error: false,
+			message: "Successfully Loged In",
+			user: req.user,
+		});
+	} else {
+		res.status(403).json({ error: true, message: "Not Authorized" });
+	}
+});
+
 router.get("/login/failed", (req, res) => {
 	res.status(401).json({
 		error: true,
@@ -27,5 +39,4 @@ router.get("/logout", (req, res) => {
         res.redirect(process.env.CLIENT_URL);
     });
 });
-
 module.exports = router;
